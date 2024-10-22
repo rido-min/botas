57c78af91c4fa5996d4333c3c6845dd122307e75
Rido <rido-min@users.noreply.github.com>
2024-10-22 23:53:57 +0000

    tests

--- packages/core/test/index.spec.js
+++ packages/core/test/index.spec.js
@@ -1,8 +1,13 @@
 import assert from 'assert/strict'
 import { describe, it } from 'node:test'
+import {getRandomGuid} from '../src/index'
 
 describe('bogus tests', () => {
   it('passes', () => {
     assert.ok(true)
+  })
+  it('guid is 36 characters long', () => {
+    const guid = getRandomGuid()
+    assert.equal(guid.length, 36)
   })
 })
