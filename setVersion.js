79a2e193c12cd0bc3dff2ad4633a8257510519db
Rido <rido-min@users.noreply.github.com>
2024-10-24 22:19:44 +0000

    override cloudbuild number

--- setVersion.js
+++ setVersion.js
@@ -2,9 +2,11 @@ import * as nbgv from 'nerdbank-gitversioning';
 import fs from 'fs';
 
 const setPackageVersionAndBuildNumber = (versionInfo) => {
+    
     // Set a build output value representing the NPM package version
     console.log("::set-output name=package_version::" + versionInfo.npmPackageVersion);
-    
+    console.log("##vso[build.updatebuildnumber]" + versionInfo.npmPackageVersion)
+
     fs.readdir('packages', { withFileTypes: true}, (err, files) => {
         if (err) {
             console.error("Failed to read the packages directory: " + err);
