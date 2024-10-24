981685cd15d8ed93f4fe3c1064bdc1c40873e6c9
Ricardo Minguez Pablos (RIDO) <rmpablos@microsoft.com>
2024-10-24 15:05:05 -0700

    setVersion

--- setVersion.js
+++ setVersion.js
@@ -1,11 +1,20 @@
-const nbgv = require("nerdbank-gitversioning");
+import * as nbgv from 'nerdbank-gitversioning';
+import fs from 'fs';
 
 const setPackageVersionAndBuildNumber = (versionInfo) => {
     // Set a build output value representing the NPM package version
     console.log("::set-output name=package_version::" + versionInfo.npmPackageVersion);
     
-    nbgv.setPackageVersion("packages/core");
-    nbgv.setPackageVersion("packages/hosting");
+    fs.readdir('packages', { withFileTypes: true}, (err, files) => {
+        if (err) {
+            console.error("Failed to read the packages directory: " + err);
+            return;
+        }
+        const folders = files
+            .filter(file => file.isDirectory())
+            .map(folder => `${folder.parentPath}/${folder.name}`);
+        folders.forEach(f => nbgv.setPackageVersion(f));
+    });
 };
 
 const handleError = (err) => console.error("Failed to update the package version number. nerdbank-gitversion failed: " + err);
